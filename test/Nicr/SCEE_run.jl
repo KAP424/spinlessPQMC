de19eeb03b5b7d25f2df1213a4364ad9a863ad08
KAP424 <kap.4116@gmail.com>
2025-11-17 14:59:38 +0800

    stable

--- test/Nicr/SCEE_run.jl
+++ test/Nicr/SCEE_run.jl
@@ -6,18 +6,18 @@ using Random
 
 
 t=1;   Lattice="HoneyComb120"    
-U=1;     Δt=0.025;     Θ=4.0;
+U=1;     Δt=0.05;     Θ=1.0;
 BatchSize=5;
 
 println("Threads: ",Threads.nthreads())
 path="C:/Users/admin/Desktop/JuliaDQMC/code/spinlessPQMC/test/Nicr/"
 # 3,6,9,12,15
-for L in [6,9,12,15]
+for L in [3,6,9,12,15]
     println("L = ",L)
     site=[L,L]
 
     model=Hubbard_Para(t,U,Lattice,site,Δt,Θ,BatchSize,"V")
-    println(model.nodes)
+    # println(model.nodes)
     Threads.@threads for i in 1:Threads.nthreads()  
         rng=MersenneTwister(time_ns()+Threads.threadid())
 
